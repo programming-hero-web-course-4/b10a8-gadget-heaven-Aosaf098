9a9c8e3156a906b6d5cca4905f0d821be680768e
Aosaf098 <aosafibad10@gmail.com>
2024-11-03 23:55:52 +0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-slate-900 text-7xl font-sora'>StarkTech</h1>
+      <h1 className='text-7xl text-prim'>StarkTech</h1>
+    </div>
+  )
+}
+
+export default App
